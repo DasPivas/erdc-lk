6cb135b7dc7c34acdfbf4978cdfeb6b9fa70be7c
Misha Pryadun <m.pryadun@sitesoft.ru>
2021-11-16 18:13:49 +0500

    feat(popup) add popup

--- assets/js/lk-popup.js
+++ assets/js/lk-popup.js
@@ -2,19 +2,44 @@ window.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('.lk-popup__evaluations-label').forEach(label => {
     label.addEventListener('click', (e) => {
       const target = e.target
-      const popupForm = target.closest('form')
+      const popupForm = document.querySelector('.lk-popup--active')
       const formLabls = popupForm.querySelectorAll('.lk-popup__evaluations-label')
+      let isPaint = true
 
       formLabls.forEach(innerLabel => {
-        console.log('5555', innerLabel)
-        if (innerLabel.classList.contains('lk-popup__evaluations-input--selected')) {
-          innerLabel.classList.remove('lk-popup__evaluations-input--selected')
+        if (innerLabel.classList.contains('lk-popup__evaluations-label--selected')) {
+          innerLabel.classList.remove('lk-popup__evaluations-label--selected')
+        }
+        if (isPaint) {
+          innerLabel.classList.add('lk-popup__evaluations-label--selected')
+        }
+        if (innerLabel === target) {
+          isPaint = false
         }
       })
-
-      target.classList.add('lk-popup__evaluations-input--selected')
-      console.log(target)
     })
   })
 
+  document.querySelectorAll('[data-for-popup]').forEach(consultationBtn => {
+
+    consultationBtn.addEventListener('click', e => {
+      e.preventDefault()
+
+      const optionPopup = consultationBtn.dataset.forPopup
+      const ticketNumber = consultationBtn.dataset.tiketNumber
+      const popup = document.querySelector(`[data-popup=${optionPopup}]`)
+
+      popup.classList.add('lk-popup--active')
+      popup.classList.add('show')
+      console.log(optionPopup,ticketNumber)
+    })
+  })
+
+  document.querySelectorAll('[data-js=popup-close]').forEach(closeBtn => {
+    closeBtn.addEventListener('click', e => {
+      const activeForm = document.querySelector('.lk-popup--active')
+
+      activeForm.classList.remove('lk-popup--active')
+    })
+  })
 })
